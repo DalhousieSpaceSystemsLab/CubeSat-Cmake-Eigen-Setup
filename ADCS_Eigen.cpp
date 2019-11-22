edf8b31374e90df38a87711733d19a0f1d745aa9
Cathy <c.song@dal.ca>
2019-11-22 15:18:14 -0400

    IGRF parsing complete
    run to see outputs!

--- ADCS_Eigen.cpp
+++ ADCS_Eigen.cpp
@@ -1,4 +1,13 @@
-// ADCS_Eigen.cpp : This file contains the 'main' function. Program execution begins and ends there.
+/*
+ * Authors: Cathy Song and...
+ * Project: Dalhousie CubeSat
+ * SubSystem: ADCS
+ * Date:   2019-11-22
+ *
+ * This file contains the main function for the ADCS module. 
+ *
+ */
+
 
 #include "pch.h"
 #include <iostream>
@@ -8,12 +17,14 @@
 #define	testparse	// outputs to terminal for testing the parse function
 
 enum returnvals{FAIL, SUCCESS};
-#define IGRF_READLINES	104
-#define MAX_MN_VALUE	13
+#define IGRF_READLINES	104						// 
+#define MAX_MN_VALUE	13						// max m and n values
 
-/* storage arrays for g and h */
-float g_nominal[MAX_MN_VALUE][MAX_MN_VALUE];	// 
-float h_nominal[MAX_MN_VALUE][MAX_MN_VALUE];	// 
+
+
+/* storage arrays for IGRF 12 parsing */
+float g_nominal[MAX_MN_VALUE][MAX_MN_VALUE];
+float h_nominal[MAX_MN_VALUE][MAX_MN_VALUE];
 float SV_g[MAX_MN_VALUE][MAX_MN_VALUE];			// secular variation of g
 float SV_h[MAX_MN_VALUE][MAX_MN_VALUE];			// secular variation of h
 
@@ -26,34 +37,66 @@ int main()
 {
 	if (Parse_IGRF()) {
 		// continue the program
+		cout << "\n\nnow we can continue with the algorithm!";
+	}
+	else {
+		cout << "unable to open the IGRF file\n";	// diagnostic
 	}
 }
 
 int Parse_IGRF(void) {
+	/************************************************************************
+	Description: This function parses the IGRF.txt file which contains the
+	gmn, hmn, and SVgmn, SVhmn values required for the IGRF algorithm.
+	
+	Author: Cathy
+
+	input: none
+	output: SUCCESS|FAIL diagnostic
+	*************************************************************************/
 	ifstream IGRFconstants;
 	IGRFconstants.open("IGRF12.txt", ifstream::in);
 
-	int m = 0, n = 0;
+	/* parsing variables */
+	int m = 0, n = 0, i = 0;
+	string garbage;
 
 	if (!IGRFconstants) {
 #ifdef testparse
-		cout << "unable to open the IGRF file\n";
 #endif
-		IGRFconstants.close();
+		IGRFconstants.close();		// close the file if opening failed
 		return FAIL;
 	}
 	else {
-#ifdef testparse
-		cout << "opened the IGRF file\n";
-#endif
-		//while (IGRFconstants) {
-		IGRFconstants >> n >> m;
-		IGRFconstants >> g_nominal[m][n];
-		IGRFconstants >> h_nominal[m][n];
-		IGRFconstants >> SV_g[m][n];
-		IGRFconstants >> SV_h[m][n];
-		cout << n << " " << m << " " << g_nominal[m][n] << " " << h_nominal[m][n] << " " << SV_g[m][n] << " " << SV_h[m][n];
-		//}
+		cout << "opened the IGRF file\n\n";	// file opening was successful
+		cout << "n (x axis), m (y axis):\n";
+		while (IGRFconstants) {
+			IGRFconstants >> n >> m;
+			IGRFconstants >> g_nominal[m][n];
+			IGRFconstants >> h_nominal[m][n];
+			IGRFconstants >> SV_g[m][n];
+			IGRFconstants >> SV_h[m][n];
+			IGRFconstants >> garbage >> i;
+
+			// interpretation f data was successful
+			//cout << n << " " << m << " " << g_nominal[m][n] << " " << h_nominal[m][n] << " " << SV_g[m][n] << " " << SV_h[m][n];
+		}
+		cout << "g values(m,n): \n";
+		for (n = 0; n < MAX_MN_VALUE; n++) {
+			for (m = 0; m < MAX_MN_VALUE; m++) {
+				cout << g_nominal[m][n] << "\t";
+			}
+			cout << "\n";
+		}
+		/* print the h values with respect to m and n*/
+		cout << "\nh values(m,n): \n";
+		for (n = 0; n < MAX_MN_VALUE; n++) {
+			for (m = 0; m < MAX_MN_VALUE; m++) {
+				cout << h_nominal[m][n] << "\t";
+			}
+			cout << "\n";
+		}
 	}
-		return SUCCESS;
+	IGRFconstants.close();	// close the file
+	return SUCCESS;
 }
