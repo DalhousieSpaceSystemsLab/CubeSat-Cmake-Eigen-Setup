6bec6962e891950cbac7e018f681dcdc1be63c9d
Cathy <c.song@dal.ca>
2019-11-22 14:37:35 -0400

    now reading a line of data

--- ADCS_Eigen.cpp
+++ ADCS_Eigen.cpp
@@ -1,5 +1,4 @@
 // ADCS_Eigen.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include "pch.h"
 #include <iostream>
@@ -13,11 +12,15 @@ enum returnvals{FAIL, SUCCESS};
 #define MAX_MN_VALUE	13
 
 /* storage arrays for g and h */
-float g_nominal[MAX_MN_VALUE][MAX_MN_VALUE];	// g[m][n]
-float h_nominal[MAX_MN_VALUE][MAX_MN_VALUE];	// h[m][n]
+float g_nominal[MAX_MN_VALUE][MAX_MN_VALUE];	// 
+float h_nominal[MAX_MN_VALUE][MAX_MN_VALUE];	// 
+float SV_g[MAX_MN_VALUE][MAX_MN_VALUE];			// secular variation of g
+float SV_h[MAX_MN_VALUE][MAX_MN_VALUE];			// secular variation of h
 
 using Eigen::Matrix3d;
 using namespace std;
+
+int Parse_IGRF(void);
 
 int main()
 {
@@ -31,16 +34,26 @@ int Parse_IGRF(void) {
 	IGRFconstants.open("IGRF12.txt", ifstream::in);
 
 	int m = 0, n = 0;
-	float
-
 
 	if (!IGRFconstants) {
 #ifdef testparse
 		cout << "unable to open the IGRF file\n";
 #endif
+		IGRFconstants.close();
 		return FAIL;
 	}
-	while (IGRFconstants) {
-		IGRFconstants>>
+	else {
+#ifdef testparse
+		cout << "opened the IGRF file\n";
+#endif
+		//while (IGRFconstants) {
+		IGRFconstants >> n >> m;
+		IGRFconstants >> g_nominal[m][n];
+		IGRFconstants >> h_nominal[m][n];
+		IGRFconstants >> SV_g[m][n];
+		IGRFconstants >> SV_h[m][n];
+		cout << n << " " << m << " " << g_nominal[m][n] << " " << h_nominal[m][n] << " " << SV_g[m][n] << " " << SV_h[m][n];
+		//}
 	}
+		return SUCCESS;
 }
